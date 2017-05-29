83e39c8ed144f8f814a4e33a1ff0b18a06314441
Dodocro <daliborsuplika@gmail.com>
2017-05-29 18:17:39 +0200

    Add files via upload

--- 3zadB.cpp
+++ 3zadB.cpp
@@ -10,55 +10,66 @@
 #include <sys/wait.h>
 
 using namespace std;
+
 typedef struct PODACI{
         int ULAZ;
         int IZLAZ;
         int M[5];
-}Shared;
-Shared *shared;
-int shmId;
-int N;
-int K;
+}Share;
+
+Share *share;
 int PUN = 0;
 int PISI = 1;
 int PRAZAN = 2;
 int SemId;
+int Sid;
+int A;
+int B;
 
-void SemGet(int n){
-        SemId = semget(IPC_PRIVATE, n, 0600);
+int SSV(int SemNum, int SemVal){
+        return semctl(SemId, SemNum, SETVAL, SemVal);
+}
+
+void Sget(int n){
+        SemId = Sget(IPC_PRIVATE, n, 0600);
 
         if(SemId == -1){
                 cout<<"Nema semafora!"<<endl;
                 exit(1);
         }
 }
-int SemSetVal(int SemNum, int SemVal){
-        return semctl(SemId, SemNum, SETVAL, SemVal);
-}
 
-int SemOp(int SemNum, int SemOp){
+int Seo(int SemNum, int Seo){
         struct sembuf SemBuf;
         SemBuf.sem_num = SemNum;
-        SemBuf.sem_op = SemOp;
+        SemBuf.sem_op = Seo;
         SemBuf.sem_flg = 0;
-        return semop(SemId, &SemBuf, 1);
+        return Seo(SemId, &SemBuf, 1);
 }
 
-void SemRemove(void){
+
+void brisi(int sig){
+        (void) shmdt((char *) share);
+        (void) shmctl(Sid, IPC_RMID, NULL);
+        exit(0);
+}
+
+void SRemove(void){
         (void) semctl(SemId, 0, IPC_RMID, 0);
 }
+
 void Proizvodac(int j){
         srand(getpid());
         int i=0;
-        for (i=0; i<K; i++){
-                SemOp(PUN,-1);
-                SemOp(PISI,-1);
-                shared->M[shared->ULAZ] = rand()%1000;
-                cout<<"Proizvodac "<<j<<" salje "<<shared->M[shared->ULAZ]<<endl;
+        for (i=0; i<B; i++){
+                Seo(PUN,-1);
+                Seo(PISI,-1);
+                share->M[share->ULAZ] = rand()%1000;
+                cout<<"Proizvodac "<<j<<" salje "<<share->M[share->ULAZ]<<endl;
                 sleep(1);
-                shared->ULAZ = (shared->ULAZ + 1) % 5;
-                SemOp(PISI, 1);
-                SemOp(PRAZAN, 1);
+                share->ULAZ = (share->ULAZ + 1) % 5;
+                Seo(PISI, 1);
+                Seo(PRAZAN, 1);
         }
         cout<<"Proizvodac "<<j<<" zavrsio sa slanjem."<<endl;;
         exit(0);
@@ -66,45 +77,41 @@ void Proizvodac(int j){
 void Potrosac(){
         int suma = 0;
         int i = 0;
-        for (i=0; i<(N*K); i++){
-                SemOp(PRAZAN,-1);
-                cout<<"Potrosac prima "<<shared->M[shared->IZLAZ]<<endl;
+        for (i=0; i<(A*B); i++){
+                Seo(PRAZAN,-1);
+                cout<<"Potrosac prima "<<share->M[share->IZLAZ]<<endl;
                 sleep(1);
-                suma += shared->M[shared->IZLAZ];
-                shared->IZLAZ = (shared->IZLAZ+1)%5;
-                SemOp(PUN, 1);
+                suma += share->M[share->IZLAZ];
+                share->IZLAZ = (share->IZLAZ+1)%5;
+                Seo(PUN, 1);
         }
         cout<<"Potrosac - suma primljenih brojeva = "<<suma<<endl;
         exit(0);
 }
-void brisi(int sig){
-        (void) shmdt((char *) shared);
-        (void) shmctl(shmId, IPC_RMID, NULL);
-        exit(0);
-}
+
 
 int main (int argc, char* argv[]) {
         if(argc < 3){
-                cout<<"Neispravni argumenti, potrebno 2: broj procesa potrosaca i broj slucajnih brojeva"<<endl;
+                cout<<"Neispravno, potrebno: broj procesa potrosaca, broj slucajnih brojeva"<<endl;
                 return 0;
         }
-        N = atoi(argv[1]);
-        K = atoi(argv[2]);
+        A = atoi(argv[1]);
+        B = atoi(argv[2]);
         sigset(SIGINT, brisi);
-        shmId = shmget(IPC_PRIVATE, sizeof(Shared), 0600);
-        if(shmId == -1){
+        Sid = shmget(IPC_PRIVATE, sizeof(Share), 0600);
+        if(Sid == -1){
                 cout<<"Nedovoljno memorije!"<<endl;
                 exit(1);
         }
-        shared = (Shared*) shmat(shmId, NULL, 0);
-        shared->ULAZ = 0;
-        shared->IZLAZ = 0;
-        SemGet(3);
-        SemSetVal(PISI, 1);
-        SemSetVal(PUN, 5);
-        SemSetVal(PRAZAN, 0);
+        share = (Share*) shmat(Sid, NULL, 0);
+        share->ULAZ = 0;
+        share->IZLAZ = 0;
+        Sget(3);
+        SSV(PISI, 1);
+        SSV(PUN, 5);
+        SSV(PRAZAN, 0);
         int k;
- for(k=0; k<=N; k++){
+ for(k=0; k<=A; k++){
                 if (fork() == 0){
                         if(k==0){
                                 Potrosac();
@@ -114,7 +121,7 @@ int main (int argc, char* argv[]) {
                 }
         }
         int i;
-        for(i=0; i<=N; i++){
+        for(i=0; i<=A; i++){
                 wait(NULL);
         }
         semctl(SemId, 0, IPC_RMID, 0);
