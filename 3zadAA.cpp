15f0e89e722ce978b55b569633de78f1b394f7d7
Dodocro <daliborsuplika@gmail.com>
2017-05-30 17:18:35 +0200

    Add files via upload

--- 3zadAA.cpp
+++ 3zadAA.cpp
@@ -14,65 +14,66 @@ using namespace std;
 int id;
 struct sem{
     sem_t s[7];
-    int broj[7];
-}*semafor;
+    int br[7];
+}*semaf;
 
-void *zadatak(int i)
-{
-     sleep(1);
-     int n = i;
-     if(n==2) sem_wait(&semafor->s[1]);
-     if(n==3) sem_wait(&semafor->s[6]);
-     if(n==4) sem_wait(&semafor->s[0]);
-     if(n==5) sem_wait(&semafor->s[2]);
-     if(n==6){
-         sem_wait(&semafor->s[3]);
-         sem_wait(&semafor->s[4]);
-         sem_wait(&semafor->s[5]);
-     }
-     int j;
-     for(j=0;j<semafor->broj[n];j++){
-                     cout<<"Izvodim zadatak "<<n+1<<": "<<j+1<<"/"<<semafor->broj[n]<<endl;
-                     sleep(1);
-                     }
-     if(n==0) sem_post(&semafor->s[0]);
-     if(n==1){
-            sem_post(&semafor->s[1]);
-            sem_post(&semafor->s[6]);
-     }
- if(n==2) sem_post(&semafor->s[2]);
-     if(n==3) sem_post(&semafor->s[3]);
-     if(n==4) sem_post(&semafor->s[4]);
-     if(n==5) sem_post(&semafor->s[5]);
-     exit(0);
-}
-void brisi_mem(int signalId) {
-    (void) shmdt((char*) semafor);
+void brisi_memoriju(int signalId) {
+    (void) shmdt((char*) semaf);
     (void) shmctl(id, IPC_RMID, NULL);
     exit(0);
+}
+
+void *zad(int x)
+{
+     sleep(1);
+     int n = x;
+     if(n==2) sem_wait(&semaf->s[1]);
+     if(n==3) sem_wait(&semaf->s[6]);
+     if(n==4) sem_wait(&semaf->s[0]);
+     if(n==5) sem_wait(&semaf->s[2]);
+     if(n==6){
+         sem_wait(&semaf->s[3]);
+         sem_wait(&semaf->s[4]);
+         sem_wait(&semaf->s[5]);
+     }
+     int y;
+     for(y=0;y<semaf->br[n];y++){
+                     cout<<"Izvodim zadatak "<<n+1<<": "<<y+1<<"/"<<semaf->br[n]<<endl;
+                     sleep(1);
+                     }
+     if(n==0) sem_post(&semaf->s[0]);
+     if(n==1){
+            sem_post(&semaf->s[1]);
+            sem_post(&semaf->s[6]);
+     }
+ if(n==2) sem_post(&semaf->s[2]);
+     if(n==3) sem_post(&semaf->s[3]);
+     if(n==4) sem_post(&semaf->s[4]);
+     if(n==5) sem_post(&semaf->s[5]);
+     exit(0);
 }
 int main() {
     id=shmget(IPC_PRIVATE, sizeof(sem), 0600);
     if(id==-1) exit(1);
-    sigset(SIGINT, brisi_mem);
-    semafor=(sem *) shmat(id, NULL, 0);
+    sigset(SIGINT, brisi_memoriju);
+    semaf=(sem *) shmat(id, NULL, 0);
     srand(time(NULL));
     rand();
-    int i;
-    for(i=0;i<7;i++){
-            sem_init(&semafor->s[i], 1, 0);
-                semafor->broj[i]=rand()%9+1;
+    int x;
+    for(x=0;x<7;x++){
+            sem_init(&semaf->s[x], 1, 0);
+                semaf->br[x]=rand()%9+1;
             }
     sleep(1);
-    for(i=0;i<7;i++){
+    for(x=0;x<7;x++){
             if(fork()==0){
-                zadatak(i);
+                zad(x);
             }
     }
- for(i=0;i<7;i++){
+ for(x=0;x<7;x++){
             wait(NULL);
     }
-    brisi_mem(0);
+    brisi_memoriju(0);
     return 0;
 }
 
