9573cac9579881f99a3ccbf504b0d0a8915a4c5c
Dodocro <daliborsuplika@gmail.com>
2017-05-30 21:29:00 +0200

    Add files via upload

--- 4b-1.cpp
+++ 4b-1.cpp
@@ -10,17 +10,17 @@
 #include <sys/wait.h>
 using namespace std;
 
-class podaci{
+class klasa{
 	public: 
-		pthread_cond_t monitorNa;
-		pthread_cond_t monitorVele;
+		pthread_cond_t monitorN;
+		pthread_cond_t monitorV;
 		pthread_mutex_t monitor;
 		int rand;
 		bool ima;
 };
-podaci *data;
+klasa *data;
 
-void init(){
+void in_it(){
 	pthread_mutexattr_t monitora;
 	pthread_mutexattr_init(&monitora);
 	pthread_mutexattr_setpshared(&monitora, PTHREAD_PROCESS_SHARED);
@@ -32,35 +32,45 @@ void init(){
 	pthread_condattr_init(&cond_attr);
 	pthread_condattr_setpshared(&cond_attr, PTHREAD_PROCESS_SHARED);
 	
-	pthread_cond_init(&data->monitorNa, &cond_attr);
-	pthread_cond_init(&data->monitorVele, &cond_attr);
+	pthread_cond_init(&data->monitorN, &cond_attr);
+	pthread_cond_init(&data->monitorV, &cond_attr);
 	
 	pthread_condattr_destroy(&cond_attr);
 	pthread_mutexattr_destroy(&monitora);
 }
 
-string komponente[] = {"monitorima", "racunalima", "tipkovnicama"};
-string uzmi[] = {"racunalo i tipkovnicu", "tipkovnicu i monitor", "monitor i racunalo"};
-bool a = true;
-
-void nakupac(int ID_nakupca) {
+void kupac(int ID_kupca) {
 		sleep(1);
     pthread_mutex_lock(&data->monitor);
     while(true){
-        while(data->rand!=ID_nakupca||!data->ima){
-			pthread_cond_wait( &data->monitorNa, &data->monitor);
+        while(data->rand!=ID_kupca||!data->ima){
+			pthread_cond_wait( &data->monitorN, &data->monitor);
 		}
 		data->ima = false;
-		cout << "Nakupac s "  << komponente[ID_nakupca] << " uzeo " << uzmi[data->rand] << "." << endl<<endl;
+		cout << "Nakupac s "  << stvari[ID_kupca] << " uzeo " << uzmi[data->rand] << "." << endl<<endl;
 		
 		sleep(1);
-		pthread_cond_signal(&(data->monitorVele));
+		pthread_cond_signal(&(data->monitorV));
     }
     pthread_mutex_unlock(&data->monitor);
 }
+
+string stvari[] = {"monitorima", "racunalima", "tipkovnicama"};
+string uzmi[] = {"racunalo i tipkovnicu", "tipkovnicu i monitor", "monitor i racunalo"};
+bool a = true;
+
+
 int ID;
 
-void veletrgovac() {
+
+
+void obrisi(int sig){
+	shmdt((char *) data);
+   	shmctl(ID, IPC_RMID, NULL);
+   	exit(0);
+}
+
+void veletrg() {
 		sleep(1);
     srand(time(NULL));
     pthread_mutex_lock(&data->monitor);
@@ -70,49 +80,43 @@ void veletrgovac() {
         cout << "Veletrgovac stavio " << uzmi[data->rand] << endl;
 		sleep(1);
 		data->ima = true;
-        pthread_cond_broadcast(&(data->monitorNa));
-        pthread_cond_wait(&data->monitorVele, &data->monitor);
+        pthread_cond_broadcast(&(data->monitorN));
+        pthread_cond_wait(&data->monitorV, &data->monitor);
     }
     pthread_mutex_unlock(&data->monitor);
 }
 
-void brisi(int sig){
-	shmdt((char *) data);
-   	shmctl(ID, IPC_RMID, NULL);
-   	exit(0);
-}
-
 int main() {
-	sigset (SIGINT, brisi);
-	sigset (SIGQUIT, brisi);
-	sigset (SIGTSTP, brisi);
+	sigset (SIGINT, obrisi);
+	sigset (SIGQUIT, obrisi);
+	sigset (SIGTSTP, obrisi);
 	
-    ID = shmget(IPC_PRIVATE, sizeof(podaci), 0600);
-    data = (podaci*) shmat(ID, NULL, 0);
+    ID = shmget(IPC_PRIVATE, sizeof(klasa), 0600);
+    data = (klasa*) shmat(ID, NULL, 0);
     data->rand = -1;
 	data->ima = false;
-    init();
+    in_it();
     
     int pid = fork();
     if(pid == 0) {
-		veletrgovac();
+		veletrg();
 	}
 	else{
 		
 		cout<<"pid veletrgovac: "<<pid<<endl;
 	}
 	pid = fork();
-    if(pid == 0) nakupac(0);
+    if(pid == 0) kupac(0);
 	else{
 		cout<<"pid nakupac0: "<<pid<<endl;
 	}
 	pid = fork();
-    if(pid == 0) nakupac(1);
+    if(pid == 0) kupac(1);
 	else{
 		cout<<"pid nakupac1: "<<pid<<endl;
 	}
 	pid = fork();
-    if(pid == 0) nakupac(2);
+    if(pid == 0) kupac(2);
 	else{
 		cout<<"pid nakupac2: "<<pid<<endl;
 	}
@@ -121,6 +125,6 @@ int main() {
     
     for(int u = 0 ; u < 4; u++) wait(NULL);
     
-    brisi(0);
+    obrisi(0);
     return 0;
 }
